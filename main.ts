a8e9a0ade9b2317c53597e2f87dc387a1e181042
carlbauermeister <96631283+carlbauermeister@users.noreply.github.com>
2022-01-23 15:24:13 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showString("hi!")
     basic.showLeds(`
         # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
         # . . . #
+        . . # . .
+        # . . . #
+        . # # # .
         `)
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    music.playTone(294, music.beat(BeatFraction.Quarter))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    basic.showString("hi!")
+    music.playMelody("E B C5 A B G A F ", Colors.Indigo)
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })
